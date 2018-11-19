a2728570297b211bcecc38630a4ff8471e0c28b8
Dumar Ojeda <dumarojeda@gmail.com>
2018-11-19 11:51:28 -0500

    Add basic methods for ToDo app, index view and Faker gem

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,27 @@
 class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to items_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+    def item_params
+      params.require(:item).permit(:item, :state)
+    end
+
 end
